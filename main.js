3c54c8d78afba1432ee1a803bc36e54dd6797a94
Marco <posta.email3001@gmail.com>
2022-12-06 23:02:16 +0100

    bonus

--- main.js
+++ main.js
@@ -3,28 +3,20 @@ const { createApp } = Vue;
 createApp({
 data() {
     return {
-        email: "",
-        emails: []
+        emails: [],
     }   
-},
-
-methods: {
-    richiestaEmail() {
-        for (let i = 0; i < 10; i++) {
-
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((result) => {
-            this.email = result.data.response;
-            this.emails.push(this.email);
-            });
-        
-        }
-    }
 },
 
 mounted() {
 
-    this.richiestaEmail();
+    for (let i = 0; i < 10; i++) {
+
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((result) => {
+            this.emails.push(result.data.response);
+        });
+    
+    }
 
 }
 
